62988f1f07b4329b15ef1a11b8d66db14279bb32
gmendozah <geovanimendoza.h@gmail.com>
2021-07-07 23:18:51 -0400

    feat: implement method channel
    - add debug impl
    - add error impl
    - implement lib on UI

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,29 +19,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await FlutterPlugin.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -51,8 +28,21 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            ElevatedButton(
+              child: Text('debug'),
+              onPressed: () {
+                FlutterPlugin.debug('this is debug');
+              },
+            ),
+            ElevatedButton(
+              child: Text('error'),
+              onPressed: () {
+                FlutterPlugin.debug('this is error');
+              },
+            ),
+          ],
         ),
       ),
     );
--- lib/flutter_plugin.dart
+++ lib/flutter_plugin.dart
@@ -1,14 +1,19 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class FlutterPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_plugin');
+  static const MethodChannel _channel = const MethodChannel('flutter_plugin');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<void> debug(String message) async {
+    await _channel.invokeMethod('debug', {
+      'message': message,
+    });
+  }
+
+  static Future<void> error(String message) async {
+    await _channel.invokeMethod('error', {
+      'message': message,
+    });
   }
 }
